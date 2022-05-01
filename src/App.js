157fccaec5d7e0764e35d9d0ddfea8da2ed5578a
MIRA-KUMAR <mira.kumar.24@gmail.com>
2022-04-30 23:52:03 -0700

    commit charts

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import LineChart from './components/LineChart';
+import './App.css';
+
+
+const App = () => {
+  return (
+      <div>
+        <LineChart />
+      </div>
+    )
+}
+
+export default App
